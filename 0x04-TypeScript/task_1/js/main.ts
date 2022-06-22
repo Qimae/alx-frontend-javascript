d14e0a91743cf9f0a7c8582a7912f24321d9fb69
Qimae <kartelbwoy@gmail.com>
2022-06-22 10:54:52 +0300

    printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,43 @@
+/* teachers interface */
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience: number;
+  yearsOfExperience?: number;
   location: string;
   [key: string]: any
 }
-
+/* directors interface */
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+/* teacher */
+const teacher1: Teacher = {
+  firstName: 'Frank',
+  lastName: 'Donald',
+  fullTimeEmployee: true,
+  location: 'New Jersey',
+  contract: false,
+}
+console.log(teacher1);
+
+/* director */
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Smith',
+  location: 'America',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+/* teacher function */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
+console.log(printTeacher('john', 'Doe'));
